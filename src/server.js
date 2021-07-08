f3eb475e94a54d4668b5020e140c7ecd056e9fe5
lawgamble <lsgrecording@gmail.com>
2021-07-08 10:49:24 -0500

    commented out server console.logs

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const listener = () => console.log(`Listening on Port ${PORT}!`);
 knex.migrate
   .latest()
   .then((migrations) => {
-    console.log("migrations", migrations);
+    // console.log("migrations", migrations);
     app.listen(PORT, listener);
   })
   .catch(console.error);
